a05a92f190846a73ae07b16bd45e0fdc53cab3b8
chendehua <chendehua@enmonster.com>
2019-10-09 14:30:07 +0800

    * update

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 #删除所有已经停止的容器
-#docker rm $(docker ps -a -q)
+docker rm $(docker ps -a -q)
 # 删除所有未打 dangling 标签的镜像
-#docker rmi $(docker images -q -f dangling=true)
+docker rmi $(docker images -q -f dangling=true)
 docker-compose build
 docker-compose down
 docker-compose up -d
